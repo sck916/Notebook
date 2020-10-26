837a953b6993141ccb4d7c1f5b091eb966fa6b7a
Steve Kavuu <mrkavuu@gmail.com>
2020-10-26 15:36:40 -0500

    final

--- server.js
+++ server.js
@@ -43,12 +43,12 @@ app.get("/api/notes", function(req, res) {
   // inside of readFIle do the push thing and push req.body to array we just read in!
   // then do a quick fs.writeFile
 app.post("/api/notes", function(req, res) {
-    fs.readFile('./db/db.json', 'utf8', function(err, data) {
+    fs.readFile('./db/db.json', 'utf8', function(err, notes) {
         console.log("we hi the /api/notes post route!!!", req.body);
 
         let oldNotes = JSON.parse(notes);
         req.body.id = oldNotes.length + 1;
-        oldData.push(req.body);
+        oldNotes.push(req.body);
         
 
         fs.writeFile('./db/db.json', JSON.stringify(oldNotes), function(err) {
@@ -59,44 +59,45 @@ app.post("/api/notes", function(req, res) {
 });
 
 app.delete("/api/notes/:id", function (req, res) {
-  fs.readFile('./db/db.json', 'utf8', function(err, data) {
+  fs.readFile('./db/db.json', 'utf8', function(err, notesData) {
     console.log('this is the ID we wanna delete',req.params);
-    let oldNotes = JSON.parse(notes);
-  // FIRST do a fs.ReadFIle to get array
-  // then do all ur good deleting stuff!!
+    let notes = JSON.parse(notesData);
+    // FIRST do a fs.ReadFIle to get array
+    // then do all ur good deleting stuff!!
 
-  // an empty array!!
-  var newNotes = [];
+    // an empty array!!
+    var newNotes = [];
 
-  //for loop thru all the notes!!
-  // we'l have if in the for loop checking
-  // if this dudes id matches the id we wanna delete
-  // fill up the empty array above for loop with the ppl we wanna keep
-  for (let i = 0; i < notes.length; i++) {
-    console.log("Id we want to  delete!!!", req.params.id);
-    console.log("is it equal to this single dudes id ??", notes[i].id);
+    //for loop thru all the notes!!
+    // we'l have if in the for loop checking
+    // if this dudes id matches the id we wanna delete
+    // fill up the empty array above for loop with the ppl we wanna keep
+    for (let i = 0; i < notes.length; i++) {
+      console.log("Id we want to  delete!!!", req.params.id);
+      console.log("is it equal to this single dudes id ??", notes[i].id);
 
-    if (parseInt(req.params.id) === notes[i].id) {
-      console.log("WE FOUND A MATCH - time to delte this dude", notes[i]);
-    } else {
-      newNotes.push(notes[i]);
+      if (parseInt(req.params.id) === notes[i].id) {
+        console.log("WE FOUND A MATCH - time to delte this dude", notes[i]);
+      } else {
+        newNotes.push(notes[i]);
+      }
     }
-  }
 
-  console.log(
-    "new notes ?? should b everyone minus dude we wanted to delete",
-    newNotes
-  );
-  // INSDEAD of doing this little reassign thing notes=Newnotes
-  // you are goin gto do a fs.writeFile instead
-//   notes = newNotes;
-//   res.json(notes);
-// });
-fs.writeFile('./db/db.json', JSON.stringify(newData), function(err) {
-    console.log(err);
-    res.json(newData);
-})
-
+      console.log(
+        "new notes ?? should be everyone minus dude we wanted to delete",
+        newNotes
+      );
+      // INSDEAD of doing this little reassign thing notes=Newnotes
+      // you are goin gto do a fs.writeFile instead
+    //   notes = newNotes;
+    //   res.json(notes);
+    // });
+    fs.writeFile('./db/db.json', JSON.stringify(newNotes), function(err) {
+        console.log(err);
+        res.json(newNotes);
+    })
+  })
+}) 
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
